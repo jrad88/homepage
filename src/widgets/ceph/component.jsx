ba9ffbdf73e7d6574d377124fe6e9ee3444e4d85
jrad88 <jerad.heck@gmail.com>
2025-03-14 15:04:41 -0700

    Formatting

--- src/widgets/ceph/component.jsx
+++ src/widgets/ceph/component.jsx
@@ -1,5 +1,4 @@
 import { useTranslation } from "next-i18next";
-
 import Container from "components/services/widget/container";
 import Block from "components/services/widget/block";
 import useWidgetAPI from "utils/proxy/use-widget-api";
